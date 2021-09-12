163824a2d6f9fbdaaae4af88c3199d0f868e8697
coldpigli <piyushdas98@gmail.com>
2021-09-12 13:35:29 +0530

    added video playing func

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from './apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component{
 
-  state = {videos: [] };
+  state = {videos: [], selectedVideo: null};
 
   onSearchSubmit = async (searchTerm) => {
      const response = await youtube.get('/search',{params: {q: searchTerm}});
      this.setState({videos: response.data.items});
   };
 
+  onVideoSelect = (video) =>{
+    this.setState({selectedVideo: video});
+  }
+
   render() {
     return <div className = "ui container">
       <SearchBar onFormSubmit = {this.onSearchSubmit}/>
-      <VideoList videos = {this.state.videos}/>
+      <VideoDetail video = {this.state.selectedVideo}/>
+      <VideoList videos = {this.state.videos} onVideoSelect={this.onVideoSelect}/>
     </div>
   }
 }
--- src/VideoList.js
+++ src/VideoList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({videos}) => {
+const VideoList = ({videos, onVideoSelect}) => {
     return (
         <div className = "ui relaxed list">
             {
             videos.map((video)=>{
-                return <VideoItem video = {video}/>
+                return <VideoItem video = {video} onVideoSelect={onVideoSelect}/>
             }
             )}
         </div>
